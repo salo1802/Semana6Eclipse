9e1cec67cd4ec4eb4978ec89095c421103b2aa3d
Salo1802 <salo1802@hotmail.com>
2021-09-25 15:49:29 -0500

    prueba

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 import processing.core.PApplet;
 
 public class Main extends PApplet{
@@ -9,9 +16,13 @@ public class Main extends PApplet{
 		
 	}
 	
+	private Socket socket; 
+	private BufferedReader reader;
+	private int x,y,r,g,b;
+	
 	@Override
 	public void settings() {
-		size(400,400);
+		size(700,700);
 		 
 		 
 	}
@@ -23,19 +34,69 @@ public class Main extends PApplet{
 	@Override
 	public void setup() {
 	
-		  
+		serveriniciar();
+		
+		x=350;
+		y=350;
+		r=255;
+		g=255;
+		b=255;
+		ellipseMode(CENTER);
 	}
 	
 @Override
 public void draw() {
-	
-	
+	background(255);
+	fill(r,g,b);
+	ellipse(x,y,100,100);
 	  
      
      
 
      
     
+	}
+
+	public void serveriniciar() {
+		new Thread(
+				()->{
+					try {
+						ServerSocket server = new ServerSocket(5000);
+						System.out.println("Esperando...");
+						socket = server.accept();
+						System.out.println("conectado");
+						InputStream is = socket.getInputStream();
+						InputStreamReader isr = new InputStreamReader(is);
+						reader = new BufferedReader(isr);
+						
+						while(true) {
+							String line = reader.readLine();
+							if(line.equals("upTrue")){
+								y-=5;
+							}
+							if(line.equals("downTrue")){
+								y+=5;
+							}
+							if(line.equals("leftTrue")){
+								x-=5;
+							}
+							if(line.equals("rightTrue")){
+								x+=5;
+							}
+							if(!line.equals("upTrue")&&!line.equals("downTrue")
+							&&!line.equals("leftTrue")&&!line.equals("rightTrue")) {
+								String[] colors = line.split(".");
+								r = Integer.parseInt(colors[0]);
+								g = Integer.parseInt(colors[1]);
+								b = Integer.parseInt(colors[2]);
+							}
+						}
+						
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}).start();
 	}
 
  }
